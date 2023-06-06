4663b17a31bc2017f821d9f909bdf2b0fc0cbb50
Andrea <andrenefer90@gmail.com>
2023-06-06 13:38:36 +0200

    Added nav links + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }];
+const routes: Routes = [
+	{ 
+		path: '', 
+		component: HomeComponent 
+	},
+	{ 
+		path: 'login', 
+		component: HomeComponent 
+	},
+	{ 
+		path: 'auth', 
+		loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) 
+	},
+	{ 
+		path: 'about-us', 
+		component: AboutUsComponent
+	},
+	{ 
+		path: 'contact', 
+		component: ContactComponent 
+	},
+	{ 
+		path: '**', 
+		component: NotFoundComponent
+	},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
