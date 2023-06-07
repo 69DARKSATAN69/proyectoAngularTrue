4c421c3e1c547923486915040cf0d7119f6e1676
JuanI <qis64129@educastur.es>
2023-06-07 16:34:58 +0200

    last default monster added to database. interface, dto class and service added to monster module. preliminary monster list done and styled.

--- src/app/monsters/monsters.component.ts
+++ src/app/monsters/monsters.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MonsterServiceService } from './services/monster-service.service';
+import { Observable, map } from 'rxjs';
+import { MonsterDTO } from './DTOclasses/monsterDTO';
 
 @Component({
   selector: 'app-monsters',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class MonstersComponent {
 
+monsterList$:Observable<MonsterDTO[]>
+  constructor(private fetcher:MonsterServiceService){
+this.monsterList$ = new Observable<MonsterDTO[]>;
+  }
+
+public getList():void{
+ this.monsterList$ = this.fetcher.getMonsterList().pipe(map(monsters => monsters.filter(monster => monster.game === 'VII')));
+}
+
+
+ngOnInit(){
+  this.getList();
+}
 }
--- src/app/monsters/monsters.module.ts
+++ src/app/monsters/monsters.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MonstersRoutingModule } from './monsters-routing.module';
 import { MonstersComponent } from './monsters.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MonsterServiceService } from './services/monster-service.service';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
@@ -11,7 +14,13 @@ import { MonstersComponent } from './monsters.component';
   ],
   imports: [
     CommonModule,
-    MonstersRoutingModule
+    MonstersRoutingModule,
+    HttpClientModule,
+    MatGridListModule,
+    MatCardModule
+  ],
+  providers: [
+    MonsterServiceService
   ]
 })
 export class MonstersModule { }
