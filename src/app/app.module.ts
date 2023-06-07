355526a42893b9f84579ae6ce9da370fdaeba887
Flavia Mabras <flaviamabras@hotmail.com>
2023-06-07 10:21:58 +0200

    imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LayoutModule } from './layout/layout.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,15 +21,21 @@ import { LayoutModule } from './layout/layout.module';
     ContactComponent,
     NotFoundComponent,
     AboutUsComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-	LayoutModule
+    LayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
