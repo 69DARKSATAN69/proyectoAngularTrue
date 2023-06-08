96791342e2459712d57c30230a4c3f6b59e7e163
Andrea <andrenefer90@gmail.com>
2023-06-08 11:21:50 +0200

    Single character component with basic code

--- src/app/characters/individual/individual.component.ts
+++ src/app/characters/individual/individual.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { CharacterDTO } from '../characterDTO/characterDTO';
+import { Observable } from 'rxjs/internal/Observable';
+
+const ELEMENT_DATA: CharacterDTO[] = [
+	{name: 'Tidus', alignment: 'Good', height: 1.70, game: 'FF X', weapon: 'Caladbolg', id: 0, image: '', recap: '', info: ''}]
 
 @Component({
   selector: 'app-individual',
@@ -6,5 +11,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./individual.component.css']
 })
 export class IndividualComponent {
+	public character;
 
+	constructor(){
+		this.character = new Observable<CharacterDTO[]>();
+	}
+
+	displayedColumns: string[] = ['name', 'alignment', 'height', 'game', 'weapon'];
+	dataSource = ELEMENT_DATA;
 }
