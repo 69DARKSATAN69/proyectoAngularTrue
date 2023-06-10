7c3bae48ef8e06fe4588d84ab7edcc740b20874c
Flavia Mabras <flaviamabras@hotmail.com>
2023-06-10 22:27:14 +0200

    finish magic-directive

--- src/app/magic/directives/magic.directive.ts
+++ src/app/magic/directives/magic.directive.ts
@@ -1,10 +1,35 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { GiphyService } from '../services/giphy.service';
 
 @Directive({
-  selector: '[appMagic]'
+  selector: '[appMagic]',
 })
 export class MagicDirective {
+  constructor(
+    private elementRef: ElementRef,
+    private giphyService: GiphyService
+  ) {}
 
-  constructor() { }
+  private originalSrc = '';
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.originalSrc = this.elementRef.nativeElement.src;
+    this.giphyService
+      .searchGiphy('final fantasy game trailer')
+      .subscribe((data) => {
+        const maxRandomIndex = data.length;
+        const randomIndex = Math.floor(Math.random() * maxRandomIndex);
+        const srcGif = data[randomIndex].images.downsized_large.url;
+        console.log(data[randomIndex].images.downsized_large.url);
+        this.setSrcImage(srcGif);
+      });
+  }
 
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setSrcImage(this.originalSrc);
+  }
+
+  private setSrcImage(url: string) {
+    this.elementRef.nativeElement.src = url;
+  }
 }
