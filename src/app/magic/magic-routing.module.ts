43673810bd43c296138c5cce07950ff8435114bf
Flavia Mabras <flaviamabras@hotmail.com>
2023-06-10 18:17:19 +0200

    add routes to individual-magic

--- src/app/magic/magic-routing.module.ts
+++ src/app/magic/magic-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, UrlSegment } from '@angular/router';
 import { GroupMagicCardComponent } from './group-magic-card/group-magic-card.component';
+import { IndividualMagicCardComponent } from './individual-magic-card/individual-magic-card.component';
 
-const routes: Routes = [{ path: '', component: GroupMagicCardComponent }];
+const magicRegex = '^spells$|^summons$';
+const regexMatcher = (url: UrlSegment[]) => {
+  return url.length === 2 && url[0].path.match(magicRegex)
+    ? {
+        consumed: url,
+        posParams: {
+          magicType: new UrlSegment(url[0].path, {}),
+          id: new UrlSegment(url[1].path, {}),
+        },
+      }
+    : null;
+};
+
+const routes: Routes = [
+  { path: '', component: GroupMagicCardComponent },
+
+  {
+    matcher: regexMatcher,
+    component: IndividualMagicCardComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
