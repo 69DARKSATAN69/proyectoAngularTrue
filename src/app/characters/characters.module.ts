1e9a1791d9c49bee6d8e82955996de8fc89a117a
Andrea <andrenefer90@gmail.com>
2023-06-13 16:44:39 +0200

    Added translator directive to character's name in individual page

--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -14,6 +14,7 @@ import {MatTableModule} from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
 import { AlignmentDirective } from './directives/alignment.directive';
 import { ToInchesPipe } from './pipes/to-inches.pipe';
+import { MagicModule } from '../magic/magic.module';
 
 
 
@@ -34,7 +35,8 @@ import { ToInchesPipe } from './pipes/to-inches.pipe';
 	MatSelectModule,
 	HttpClientModule,
 	MatTableModule,
-	MatButtonModule
+	MatButtonModule,
+	MagicModule
   ],
   providers: [
 	CharacterServiceService
